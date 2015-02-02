3a3449955ab72062b1f871d6adcc4f6a60e7eb0d
Agata Filho <agata78@gmail.com>
2015-02-02 14:04:28 -0500

    Remove dead code.

--- py_problems/lists_strings17.py
+++ py_problems/lists_strings17.py
@@ -5,20 +5,10 @@ def max_len(xs):
         r.append(len(x))
     return max(r)
 
-
-def print_rect_2(xs, i):
-    """(list, int) -> NoneType"""
-    for x in xs:
-        fmt = '{} {:'+ str(i) +'} {}'
-        return fmt.format('*', x, '*')
-
 def print_rect(x, i):
     """(list, int) -> NoneType"""
     fmt = '{} {:'+ str(i) +'} {}'
     return fmt.format('*', x, '*')
-
-
-
 
 xs = ['agata', 'jo', 'tara']
 i = max_len(xs)
